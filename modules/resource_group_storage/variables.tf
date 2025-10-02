86308b94ea48962d4a06e1e6b5f0d73add26f83b
Alina Bondarenko <alinamoskovchuk18@gmail.com>
2025-10-02 14:26:32 +0300

    2

--- modules/resource_group_storage/variables.tf
+++ modules/resource_group_storage/variables.tf
@@ -1,17 +1,14 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
-  type        = string
   default     = "example-resources"
 }
 
 variable "storage_account_name" {
   description = "The name of the storage account"
-  type        = string
   default     = "examplestorageaccount"
 }
 
 variable "location" {
   description = "The location for all resources."
-  type        = string
   default     = "East US"
 }
